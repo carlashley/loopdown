511f6a95f8d5cde4a79faa796d81728920df5b0f
carlashley <git@macapakr.com>
2022-09-18 17:24:10 +1000

    Add support for fallback to Apple for property list files

--- README.md
+++ README.md
@@ -5,32 +5,57 @@ Intended as a generic replacement for `appleloops` to facilitate _downloading_ c
 ## Requirements
 Python 3.5+ (Python3 3.9 recommended).
 
+Note: Standalone versions are available in the `./dist` directory for either the `x86_64` or `arm64` platforms. A Python framework should not be required to use these versions.
+ - These are built using `nuitka3` and have only had basic testing done on macOS 12, support is provided _as is_.
+
 ## Usage
 ```
 [jappleseed@loopback]:loopdown # ./loopdown.py -h
-usage: loopdown.py [-h] [-n] [-m] [-o] [-d [path]] [-p [[file] ...] | -t
-                   [file]] [--retries [retry]] [-v]
+usage: loopdown [-h] [-n] [-m] [-o] [-d [path]] [-p [[file] ...] | -t [file]] [--retries [retry]] [-v]
+
+loopdown v1.0.20220918 is a utility to download the audio content packages
+from Apple so they can be deployed with third party app management tools
+such as 'munki' or 'JAMF'.
+ 
+Basic usage example:
+  loopdown -p garageband1021.plist -m -d ~/Desktop/appleaudiocontent
+ 
+If a specified property list file is not found at the specified path, then
+loopdown will fallback to a remote version of that property list file, if
+available.
+ 
+The specific property list files are usually found in the app bundle:
+  /Applications/[app]/Contents/Resources/[appname]NNNN.plist
+'NNNN' represents a 'version' number.
+ 
+For example:
+ 
+  /Applications/GarageBand.app/Contents/Resources/garageband1021.plist
+ 
+Note: The property list file does not always get updated with each audio app
+release.
+ 
+For the most recent version of loopdown:
+  git: https://github.com/carlashley/loopdown
 
 options:
   -h, --help            show this help message and exit
   -n, --dry-run         performs a dry run (prints each URL to screen)
-  -m, --mandatory       process mandatory packages, either this or
-                        -o/--optional is required
-  -o, --optional        process optional packages, either this or
-                        -m/--mandatory is required
+  -m, --mandatory       process mandatory packages, either this or -o/--optional is required
+  -o, --optional        process optional packages, either this or -m/--mandatory is required
   -d [path], --destination [path]
-                        path to download destination, folder path will be
-                        created automatically if it does not exist; defaults
-                        to '/tmp/loopdown' for non dry-runs
+                        path to download destination, folder path will be created
+                        automatically if it does not exist; defaults to '/tmp/loopdown'
+                        for non dry-runs
   -p [[file] ...], --property-list [[file] ...]
-                        path to property list file/s to process, must be valid
-                        file path and property list file; required
+                        path to property list file/s to process, must be valid file path
+                        and property list file; required
   -t [file], --text-file [file]
-                        download audio content packages from URLs in a text
-                        file
+                        download audio content packages from URLs in a text file
   --retries [retry]     maximum number of retries (default 3)
   -v, --version         show program's version number and exit
-
+[jappleseed@loopback]:loopdown #
+[jappleseed@loopback]:loopdown #
 [jappleseed@loopback]:loopdown #
 [jappleseed@loopback]:loopdown # ./loopdown.py -n -p ~/Downloads/ms.plist -m -o
 https://audiocontentdownload.apple.com/lp10_ms3_content_2013/GarageBandBasicContent.pkg
--- loopdown.py
+++ loopdown.py
@@ -2,27 +2,51 @@
 """Download or print out the URL to the additional content packages that Apple
 provides for the GarageBand, Logic Pro, and MainStage audio programs.
 
-usage: loopdown.py [-h] [-n] [-m] [-o] [-d [path]] -p [[file] ...]
-                   [--retries [retry]] [-v]
+usage: loopdown.py [-h] [-n] [-m] [-o] [-d [path]] [-p [[file] ...] | -t [file]] [--retries [retry]] [-v]
+
+loopdown v1.0.20220918 is a utility to download the audio content packages
+from Apple so they can be deployed with third party app management tools
+such as 'munki' or 'JAMF'.
+
+Basic usage example:
+  loopdown -p garageband1021.plist -m -d ~/Desktop/appleaudiocontent
+
+If a specified property list file is not found at the specified path, then
+loopdown will fallback to a remote version of that property list file, if
+available.
+
+The specific property list files are usually found in the app bundle:
+  /Applications/[app]/Contents/Resources/[appname]NNNN.plist
+'NNNN' represents a 'version' number.
+
+For example:
+
+  /Applications/GarageBand.app/Contents/Resources/garageband1021.plist
+
+Note: The property list file does not always get updated with each audio app
+release.
+
+For the most recent version of loopdown:
+  git: https://github.com/carlashley/loopdown
 
 options:
   -h, --help            show this help message and exit
   -n, --dry-run         performs a dry run (prints each URL to screen)
-  -m, --mandatory       process mandatory packages, either this or
-                        -o/--optional is required
-  -o, --optional        process optional packages, either this or
-                        -m/--mandatory is required
+  -m, --mandatory       process mandatory packages, either this or -o/--optional is required
+  -o, --optional        process optional packages, either this or -m/--mandatory is required
   -d [path], --destination [path]
-                        path to download destination, folder path will be
-                        created automatically if it does not exist; defaults
-                        to '/tmp/loopdown' for non dry-runs
+                        path to download destination, folder path will be created
+                        automatically if it does not exist; defaults to '/tmp/loopdown'
+                        for non dry-runs
   -p [[file] ...], --property-list [[file] ...]
-                        path to property list file/s to process, must be valid
-                        file path and property list file; required
+                        path to property list file/s to process, must be valid file path
+                        and property list file; required
+  -t [file], --text-file [file]
+                        download audio content packages from URLs in a text file
   --retries [retry]     maximum number of retries (default 3)
   -v, --version         show program's version number and exit
 
-loopdown v1.0.1 is provided 'as is'; licensed under the Apache License 2.0
+loopdown is provided 'as is'; licensed under the Apache License 2.0
 """
 import argparse
 import errno
@@ -37,14 +61,31 @@ from typing import Any, Dict, List, Optional
 APPLE_BASE_URL = "https://audiocontentdownload.apple.com"
 APPLE_FEED_URL = "https://audiocontentdownload.apple.com/lp10_ms3_content_2016"
 SUFFIXES = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
-VERSION = "1.0.2"
+VERSION = "1.0.20220918"
 USERAGENT = f"loopdown/{VERSION}"
 DEFAULT_DEST = "/tmp/loopdown"
+ARGPARSE_DESC = (f"loopdown v{VERSION} is a utility to download the audio content packages\n"
+                 "from Apple so they can be deployed with third party app management tools\n"
+                 "such as 'munki' or 'JAMF'.\n \n"
+                 "Basic usage example:\n"
+                 "  loopdown -p garageband1021.plist -m -d ~/Desktop/appleaudiocontent\n \n"
+                 "If a specified property list file is not found at the specified path, then\n"
+                 "loopdown will fallback to a remote version of that property list file, if\n"
+                 "available.\n \n"
+                 "The specific property list files are usually found in the app bundle:\n"
+                 "  /Applications/[app]/Contents/Resources/[appname]NNNN.plist\n"
+                 "'NNNN' represents a 'version' number.\n \n"
+                 "For example:\n \n"
+                 "  /Applications/GarageBand.app/Contents/Resources/garageband1021.plist\n \n"
+                 "Note: The property list file does not always get updated with each audio app\n"
+                 "release.\n \n"
+                 "For the most recent version of loopdown:\n"
+                 "  git: https://github.com/carlashley/loopdown")
 
 
 def arguments() -> argparse.Namespace:
     """Create the command line arguments."""
-    p = argparse.ArgumentParser()
+    p = argparse.ArgumentParser(description=ARGPARSE_DESC, formatter_class=argparse.RawTextHelpFormatter)
     aa = p.add_argument
     me = p.add_mutually_exclusive_group().add_argument
     name = Path(sys.argv[0]).name
@@ -74,8 +115,8 @@ def arguments() -> argparse.Namespace:
        default=DEFAULT_DEST,
        metavar="[path]",
        help=("path to download destination, folder path will be created"
-             " automatically if it does not exist; defaults to"
-             f" {DEFAULT_DEST!r} for non dry-runs"),
+             "\nautomatically if it does not exist; defaults to"
+             f" {DEFAULT_DEST!r}\nfor non dry-runs"),
        required=False)
 
     me("-p", "--property-list",
@@ -83,7 +124,7 @@ def arguments() -> argparse.Namespace:
        dest="plists",
        metavar="[file]",
        help=("path to property list file/s to process, must be valid file path"
-             " and property list file; required"),
+             "\nand property list file; required"),
        required=False)
 
     me("-t", "--text-file",
@@ -117,6 +158,8 @@ def arguments() -> argparse.Namespace:
                 files.append(Path(fp))
             elif fp.is_dir():
                 files.extend([fn for fn in fp.rglob("*.plist")])
+            else:
+                files.append(Path(fp))  # Allow for fallback
 
         args.plists = files
 
@@ -142,8 +185,11 @@ def arguments() -> argparse.Namespace:
 
 
 def bytes2hr(b: str, counter: int = 0, byteblock: int = 1024) -> Optional[str]:
-    """Convert bytes to human readable value
-    :param b: bytes as string value"""
+    """Convert bytes to human readable value.
+
+    :param b: bytes as string value
+    :param counter: count from position index
+    :param byteblock: bytes per KB"""
     b = int(b)
 
     while b > byteblock and counter < len(SUFFIXES):
@@ -154,35 +200,73 @@ def bytes2hr(b: str, counter: int = 0, byteblock: int = 1024) -> Optional[str]:
     return f"{b:.2f}{suffix}"
 
 
-def download(url: str, dest: Path, retries: str = "3") -> None:
+def download(url: str, dest: Path, retries: str = "3", compressed: bool = False) -> None:
     """Download the URL to the supplied destination.
+
     :param url: URL to download
     :param dest: destination folder path to download the file to
-    :param retries: number of retries to attempt if download fails"""
-    dest = dest.joinpath(urlparse(url).path.lstrip("/"))
+    :param retries: number of retries to attempt if download fails
+    :param compressed: indicates if the remote resource is compressed"""
+    dest = dest if dest.suffix == ".plist" else dest.joinpath(urlparse(url).path.lstrip("/"))
     cmd = ["/usr/bin/curl", "-L", "-C", "-", "--retry", retries,
            "--user-agent", USERAGENT, "--progress-bar", "--create-dirs", url,
            "-o", str(dest)]
 
+    if compressed:
+        cmd.append("--compressed")
+
+    if dest.suffix == ".plist":
+        cmd.append("--silent")
+
     subprocess.run(cmd)
 
 
-def download_size(url: str) -> Optional[str]:
-    """Get the content-length header"""
+def get_header(url: str, header: str) -> Optional[str]:
+    """Get header and its value.
+
+    :param url: url
+    :param header: header to return value of"""
     cmd = ["/usr/bin/curl", "-s", "-I", url]
-    cl = "Content-Length: "
     p = subprocess.run(cmd, capture_output=True, encoding="utf-8")
 
     if p.returncode == 0:
-        splitlines = p.stdout.strip().splitlines()
-        size = "".join([line.replace(cl, "") for line in splitlines
-                        if cl in line])
-        return bytes2hr(size)
+        return "".join([ln.replace(header, "") for ln in p.stdout.strip().splitlines()
+                        if header.lower() in ln.lower()])
 
 
-def pull_urls(data: Dict[Any, Any], mandatory: bool = False,
+def download_size(url: str, header: str) -> Optional[str]:
+    """Get the content-length header.
+
+    :param url: url to calculate size of"""
+    size = get_header(url=url, header="content-length")
+    return bytes2hr(size) if size else None
+
+
+def status_code(url: str) -> Optional[int]:
+    """HTTP status code of remote resource.
+
+    :param urul: url"""
+    cmd = ["/usr/bin/curl", "-s", "-o", "/dev/null", "-I", "-w", "%{http_code}", "https://example.org"]
+    p = subprocess.run(cmd, capture_output=True, encoding="utf-8")
+
+    if p.returncode == 0:
+        return int(p.stdout.strip())
+    else:
+        raise Exception(f"Error: {' '.join(cmd)} exit code: {p.returncode}")
+
+
+def encoding_type(url: str) -> bool:
+    """Return encoding type (for exmaple 'gzip') if the remote resource has the 'content-encoding' header.
+
+    :param url: url of remote resource"""
+    return get_header(url=url, header="content-encoding")
+
+
+def pull_urls(data: Dict[Any, Any],
+              mandatory: bool = False,
               optional: bool = False) -> Optional[List[str]]:
     """Pulls URL's of packages to download from the supplied data source.
+
     :param data: dictionary of loop objects to process
     :param mandatory: process mandatory packages
     :param optional: process optional packages"""
@@ -209,6 +293,7 @@ def pull_urls(data: Dict[Any, Any], mandatory: bool = False,
 
 def readplist(fp: Path) -> Optional[Dict[Any, Any]]:
     """Read the specified property list file.
+
     :param fp: path to the property list file to read."""
     try:
         with fp.open("rb") as f:
@@ -225,8 +310,12 @@ def readplist(fp: Path) -> Optional[Dict[Any, Any]]:
         sys.exit(errno.ENOENT)
 
 
-def readfile(fp: Path, ext: str = ".pkg") -> Optional[List[str]]:
-    """Read a text file for URL's"""
+def readfile(fp: Path,
+             ext: str = ".pkg") -> Optional[List[str]]:
+    """Read a text file for URL's.
+
+    :param fp: path to text file
+    :param ext: package file extension"""
     try:
         with fp.open("r") as f:
             return {line.strip() for line in f.readlines()
@@ -236,18 +325,48 @@ def readfile(fp: Path, ext: str = ".pkg") -> Optional[List[str]]:
         sys.exit(errno.ENOENT)
 
 
+def fallback(fp: Path) -> Optional[Dict[Any, Any]]:
+    """Perform fallback download.
+
+    :param fp: plist file path"""
+    url = f"{APPLE_FEED_URL}/{fp.name}"
+    sc = status_code(url)
+    print(f"Falling back to {url!r}")
+
+    if sc == 200:
+        tmp_dest = Path(f"/tmp/{fp.name}")
+        compressed = encoding_type(url) == "gzip"
+        download(url=url, dest=tmp_dest, compressed=compressed)
+
+        if tmp_dest.exists():
+            try:
+                data = readplist(tmp_dest)
+                tmp_dest.unlink(missing_ok=True)
+                return data
+            except plistlib.InvalidFileException:
+                print(f"Error: {url!r} is not a valid XML file, skipping.")
+                tmp_dest.unlink(missing_ok=True)
+
+
 def main():
+    """Main"""
     args = arguments()
     package_urls = set()
     counter = 1
 
     if args.plists:
         for plist in args.plists:
-            data = readplist(plist)
-            urls = pull_urls(data, args.mandatory, args.optional)
+            if not plist.exists():
+                data = fallback(plist)
+            else:
+                data = readplist(plist)
 
-            for url in urls:
-                package_urls.add(url)
+            # Only carry on if 'data' exists.
+            if data:
+                urls = pull_urls(data, args.mandatory, args.optional)
+
+                for url in urls:
+                    package_urls.add(url)
     elif args.text_file:
         package_urls = readfile(args.text_file)
 
