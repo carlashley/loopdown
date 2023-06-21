ba32b42e7fe6ec22c1ef8d2917f666be2a748610
carlashley <git@macapakr.com>
2023-06-21 20:49:22 +1000

    Document hidden arguments and add a means of showing hidden argument help

--- src/ldilib/clargs.py
+++ src/ldilib/clargs.py
@@ -4,6 +4,7 @@ import sys
 import tempfile
 
 from pathlib import Path
+from typing import Optional
 from . import _version_string
 from .utils import (
     is_root,
@@ -14,7 +15,7 @@ from .utils import (
 )
 
 
-def arguments() -> argparse.Namespace:
+def arguments(args: Optional[list] = None) -> argparse.Namespace:
     """Construct the arguments for command line usage."""
     applications = ["all", "garageband", "logicpro", "mainstage"]
     app_examplesc_str = ", ".join(f"'{app}'" for app in applications)
@@ -23,7 +24,20 @@ def arguments() -> argparse.Namespace:
     log_levels = ["info", "debug"]
     log_levels_str = ", ".join(f"'{lvl}'" for lvl in log_levels)
 
-    parser = argparse.ArgumentParser()
+    sa = argparse.ArgumentParser(add_help=False)  # For "super args!"
+    sa.add_argument(
+        "--advanced-help",
+        action="store_true",
+        dest="show_all_help",
+        required=False,
+        help=(
+            "show hidden arguments; note, not all of these arguments should be directly modified,"
+            " use these at your own risk"
+        ),
+    )
+    sa_args, _ = sa.parse_known_args(args)
+    parser = argparse.ArgumentParser(parents=[sa])
+
     a = parser.add_argument
     app = parser.add_mutually_exclusive_group().add_argument
     dwn = parser.add_mutually_exclusive_group().add_argument
@@ -139,7 +153,10 @@ def arguments() -> argparse.Namespace:
         action="store_true",
         dest="force",
         required=False,
-        help="forcibly performs the selected options regardless of pre-existing installations/downloads, etc",
+        help=(
+            "forcibly performs the selected options regardless of pre-existing installations/downloads, etc;"
+            " this will not force downloads/installs where fetching the audio content fails for various reasons"
+        ),
     )
 
     a(
@@ -170,41 +187,166 @@ def arguments() -> argparse.Namespace:
     # -- Hidden arguments here... THESE SHOULD NOT BE USED! -- #
     # This is a hidden argument that defines the base url used for pulling property list files that are
     # not present locally
+    base_path_def = "https://audiocontentdownload.apple.com/lp10_ms3_content_2016/"
     a(
         "--feed-base-url",
         dest="feed_base_url",
-        default="https://audiocontentdownload.apple.com/lp10_ms3_content_2016/",
+        default=base_path_def,
+        metavar="[path]",
         required=False,
-        help=argparse.SUPPRESS,
+        help=(
+            argparse.SUPPRESS
+            if not sa_args.show_all_help
+            else (
+                "specify the default base url path to use when fetching remote property list files, this"
+                " url absolutely should not be modified unless you know what you are doing; default is"
+                f" {base_path_def!r}"
+            )
+        ),
+    )
+
+    # This is a hidden argument for property list file discovery
+    dwn(
+        "--discover-plists",
+        action="store_true",
+        dest="discover_plists",
+        required=False,
+        help=(
+            argparse.SUPPRESS
+            if not sa_args.show_all_help
+            else (
+                "discover property list files for GarageBand, Logic Pro X, and MainStage 3 that Apple"
+                "has released content for"
+            )
+        ),
+    )
+
+    # This is a hidden argument for property list file discovery start/end range
+    disc_plists_def = [0, 99]
+    disc_plists_def_str = ", ".join(f"'{val}'" for val in disc_plists_def)
+    a(
+        "--discover-plists-range",
+        nargs=2,
+        dest="discover_plists_range",
+        default=disc_plists_def,
+        type=int,
+        metavar="[value]",
+        required=False,
+        help=(
+            argparse.SUPPRESS
+            if not sa_args.show_all_help
+            else ("specify the start and end range for property list discovery; defaults to" f" {disc_plists_def_str}")
+        ),
     )
 
     # This is a hidden argument that defines the default download destination for all package downloads
     # (except for downloaded property list files which are downloaded to a temporary working folder in the
     # '/private/var/folders' directory)
+    packages_dld_def = Path("/tmp/loopdown")
     a(
         "--default-packages-download-dest",
         dest="default_packages_download_dest",
-        default=Path("/tmp/loopdown"),
+        default=packages_dld_def,
+        type=Path,
+        metavar="[path]",
         required=False,
-        help=argparse.SUPPRESS,
+        help=(
+            argparse.SUPPRESS
+            if not sa_args.show_all_help
+            else (
+                "specify the default working path where all audio content packages will be downloaded to,"
+                " this path absolutely should not be modified unless you know what you are doing;"
+                f" default is {str(packages_dld_def)!r}"
+            )
+        ),
     )
 
     # This is a hidden argument that defines the default download destination for all 'working' downloads
     # of property list files which are downloaded to a temporary working folder in the
     # '/private/var/folders' directory)
+    working_path_def = Path(tempfile.gettempdir()).joinpath("loopdown")
     a(
         "--default-working-download-dest",
         dest="default_working_download_dest",
-        default=Path(tempfile.gettempdir()).joinpath("loopdown"),
+        default=working_path_def,
+        type=Path,
+        metavar="[path]",
         required=False,
-        help=argparse.SUPPRESS,
+        help=(
+            argparse.SUPPRESS
+            if not sa_args.show_all_help
+            else (
+                "specify the default working path where all temporarily fetched property list files will"
+                "be downloaded to (this directory is cleaned up after successful exit), this path absolutely"
+                f" should not be modified unless you know what you are doing; default is {str(working_path_def)!r}"
+            )
+        ),
+    )
+
+    # This is a hidden argument that defines the default maximum number of retry attempts for all
+    # curl requests.
+    retry_max_def = "5"
+    a(
+        "--max-retries",
+        dest="max_retries",
+        default=retry_max_def,
+        metavar="[retries]",
+        required=False,
+        help=(
+            argparse.SUPPRESS
+            if not sa_args.show_all_help
+            else (
+                "specify a maximum number of retries for all curl subprocess calls; default is"
+                f" {retry_max_def} attempts"
+            )
+        ),
+    )
+
+    # This is a hidden argument that defines the default maximum amount of time for all retry
+    # attempts for all curl requests.
+    retry_timeout_def = "60"
+    a(
+        "--max-retry-time-limit",
+        dest="max_retry_time_limit",
+        default=retry_timeout_def,
+        metavar="[seconds]",
+        required=False,
+        help=(
+            argparse.SUPPRESS
+            if not sa_args.show_all_help
+            else (
+                "specify a maximum amount of time that before retries fail for all curl subprocess calls"
+                f"; default is {retry_timeout_def} seconds"
+            )
+        ),
+    )
+
+    # This is a hidden argument that allows addiontal arguments to be passed to curl for all curl requests.
+    a(
+        "--additional-curl-args",
+        nargs="+",
+        dest="proxy_args",
+        metavar="[arg]",
+        required=False,
+        help=(
+            argparse.SUPPRESS
+            if not sa_args.show_all_help
+            else (
+                "additional arguments to pass to all curl subprocess calls; do not use this unless"
+                "you know what you are doing"
+            )
+        ),
     )
 
     if not len(sys.argv) > 1:
         parser.print_help(sys.stderr)
         sys.exit(1)
 
-    args = parser.parse_args()
+    if sa_args.show_all_help:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+    else:
+        args = parser.parse_args()
 
     # ANy one of these specific arguments are required, possibly in combination with other arguments
     reqd_args = [
@@ -213,6 +355,7 @@ def arguments() -> argparse.Namespace:
         args.mandatory,
         args.optional,
         args.create_mirror,
+        args.discover_plists,
     ]
     reqd_arg_strs = [
         "-a/--apps",
@@ -221,6 +364,10 @@ def arguments() -> argparse.Namespace:
         "-o/--optional",
         "--create-mirror",
     ]
+
+    # Ignore dry run when discovering property list files.
+    if args.dry_run and args.discover_plists:
+        args.dry_run = False
 
     if args.install and not is_root():
         parser.print_usage(sys.stderr)
@@ -243,7 +390,7 @@ def arguments() -> argparse.Namespace:
         )
         sys.exit(1)
 
-    if not (args.apps or args.plists):
+    if not args.discover_plists and not (args.apps or args.plists):
         parser.print_usage(sys.stderr)
         print(
             f"{name}: error: one of the following arguments is required: -a/--apps or -p/--plists",
@@ -251,15 +398,15 @@ def arguments() -> argparse.Namespace:
         )
         sys.exit(1)
 
-    if not args.install and not args.create_mirror:
+    if not args.discover_plists and not args.install and not args.create_mirror:
         parser.print_usage(sys.stderr)
         print(
-            f"{name}: error: one of the following arguments is required: -i/--installor or --create-mirror",
+            f"{name}: error: one of the following arguments is required: -i/--install or --create-mirror",
             file=sys.stderr,
         )
         sys.exit(1)
 
-    if not (args.mandatory or args.optional):
+    if not args.discover_plists and not (args.mandatory or args.optional):
         parser.print_usage(sys.stderr)
         print(
             f"{name}: error: one of or both the following arguments are required: -o/--optional, -m/--mandatory",
@@ -292,8 +439,9 @@ def arguments() -> argparse.Namespace:
             )
             sys.exit(1)
 
-    # Now set default values for certain argument parameters
-    if not (args.pkg_server and args.cache_server):
+    # Now set default values for certain argument parameters; specifically if a package server mirror URL is not
+    # provided, or if a cache server _is_ provided.
+    if not args.pkg_server or args.cache_server:
         args.pkg_server = args.feed_base_url  # set the default Apple package server
 
     # Make sure the package server ends in an expected directory path
@@ -303,5 +451,23 @@ def arguments() -> argparse.Namespace:
     # Replace the list of apps if "all" is in the argument parameter with the apps to check for
     if args.apps and "all" in args.apps:
         args.apps = ["garageband", "logicpro", "mainstage"]
+
+    if args.discover_plists_range and not args.discover_plists:
+        parser.print_usage(sys.stderr)
+        print(
+            f"{name}: error: --discover-plists-range not allowed without argument: --discovery-plists",
+            file=sys.stderr,
+        )
+        sys.exit(1)
+    elif args.discover_plists_range and args.discover_plists:
+        _min, _max = args.discover_plists_range
+
+        if _min >= _max:
+            parser.print_usage(sys.stderr)
+            print(
+                f"{name}: error: --discover-plists-range values must be in 'min' 'max' order;",
+                file=sys.stderr,
+            )
+            sys.exit(1)
 
     return args
